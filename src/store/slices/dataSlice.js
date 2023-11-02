65630c290ac12054acddd399757dbc702f158e54
Ali Raza <aliraza.amanat929@gmail.com>
2023-11-02 12:06:32 +0500

    URL update

--- src/store/slices/dataSlice.js
+++ src/store/slices/dataSlice.js
@@ -9,7 +9,7 @@ const initialState = {
 
 export const fetchSimilarDocs = createAsyncThunk("data", async (params) => {
   console.log({ params }); //{q: "Ali"}
-  const apiUrl = "https://h20vrbjcl8.execute-api.us-east-1.amazonaws.com/genai-app-poc-ApiStage/api/v1/llm/rag";
+  const apiUrl = "https://y2m9s8z8og.execute-api.us-east-1.amazonaws.com/genai-app-poc-ApiStage/api/v1/llm/rag";
 
   const headers = {
     "Content-Type": "application/json",
