5a4beb1ee6cb710db49abb615d9de61d7212311e
Sam J Zero <saamjzero@hotmail.com>
2024-01-16 18:59:25 +0500

    q->question

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,11 @@ import { Form, Formik, Field, ErrorMessage } from "formik";
 import { useSelector, useDispatch } from "react-redux";
 import { getIsLoggedIn, getIdToken } from "../store/slices/authSlice";
 import { useNavigate } from "react-router-dom";
-import { fetchSimilarDocs, getData, getDataSliceStatus } from "../store/slices/dataSlice";
+import {
+  fetchSimilarDocs,
+  getData,
+  getDataSliceStatus,
+} from "../store/slices/dataSlice";
 // import DisplayData from "../components/HomeComponents/DisplayData";
 import Card from "../components/UI/Card";
 import { dataSliceActions } from "../store/slices/dataSlice";
@@ -62,7 +66,9 @@ const HomePage = () => {
     cardContent = (
       <Card className="bg-secondary p-7 ">
         <p className="text-primary pb-2 font-bold">Error Fetching data</p>
-        <p className="text-primary pl-4">This may occur due to following reasons</p>
+        <p className="text-primary pl-4">
+          This may occur due to following reasons
+        </p>
         <ol className="pl-8 list-decimal text-primary">
           <li>Your interent connection is disabled</li>
           <li>You may not be authorized, log back in</li>
@@ -77,7 +83,15 @@ const HomePage = () => {
       <Formik
         initialValues={initialValues}
         validationSchema={searchItemSchema}
-        onSubmit={(values) => dispatch(fetchSimilarDocs({ q: values.searchTerm, verbose: true, IdToken: IdToken }))}
+        onSubmit={(values) =>
+          dispatch(
+            fetchSimilarDocs({
+              question: values.searchTerm,
+              verbose: true,
+              IdToken: IdToken,
+            })
+          )
+        }
       >
         <Form className="felx items-center justify-center sm:px-9 md:px-60 px- pt-10 mb-5">
           <div className="flex items-center justify-center">
@@ -94,7 +108,11 @@ const HomePage = () => {
               {dataSliceStatus === "pending" ? "Loading..." : "Search"}
             </button>
           </div>
-          <ErrorMessage name="searchTerm" component={InputTextError} className="error" />
+          <ErrorMessage
+            name="searchTerm"
+            component={InputTextError}
+            className="error"
+          />
         </Form>
       </Formik>
       {cardContent}
